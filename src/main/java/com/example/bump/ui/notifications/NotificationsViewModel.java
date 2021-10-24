ebad171c7961a864ffc75c1f9b26dfaf82a44813
aarun41 <93056388+aarun41@users.noreply.github.com>
2021-10-24 01:03:48 -0400

    Add files via upload

---
+++ src/main/java/com/example/bump/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.bump.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("No notifications!");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
